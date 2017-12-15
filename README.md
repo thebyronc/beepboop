27925a5249d527f0db0b2e43b3467eddc429d27a
Byron Chang <thebyronc@gmail.com>
2017-12-14 21:41:49 -0800

    add ability to print all number in range with exceptions

--- README.md
+++ README.md
@@ -28,11 +28,11 @@ This project is available at the following locations:
 
 | Core Behavior | Input | Expected Output |
 | :---     |      :---:     |     :---:     |
-| It takes a number and counts to a range of numbers up to the number provided | "2" or "24" | "0, 1, 2" or "0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 ,20 , 21, 22, 23, 24"    |
 | Numbers that contain a 1 are replaced with "Boop!" | "1" or "511" | "Boop!" |
 | Numbers that contain a 0 are replaced with "Beep!" |  "0" or "200" | "Beep!" |
 | Numbers that contain a 1 and a 0 are replaced with "Beep!" |  "101" or "201" | "Beep!" |
 | Numbers divisible by 3 are replaced with "I'm sorry, Dave. I'm afraid I can't do that." | "3" or "396" | "I'm sorry, Dave. I'm afraid I can't do that." |
+| It takes a number and counts to a range of numbers up to the number provided | "2" or "24" | "0, 1, 2" or "0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 ,20 , 21, 22, 23, 24"    |
 | Numbers that contain a 1 and/or a 0, and divisible by 3 are replaced with "I'm sorry, Dave. I'm afraid I can't do that." | "12" or "30" or "102" | "I'm sorry, Dave. I'm afraid I can't do that." |
 | A user should be able to enter a new number and see new results over and over again. | -  | - |
 
--- js/scripts.js
+++ js/scripts.js
@@ -6,29 +6,47 @@ var countNumbers = function(inputName, inputNumber) {
   } else {
     console.log("I guess I'm calling you Dave.");
   }
+
+  var numberPrints = [];
+  for (i = 0; i <= parseInt(inputNumber); i++) {
+    numberPrints[i] = i;
+  }
+  // bot image
   if (parseInt(inputNumber) === 0) {
-    results[0] = "Beep!";
     results[1] = "img/bot_l.png";
     results[2] = "BEEP!"
+    results[3] = ""
   } else if (parseInt(inputNumber) % 3 === 0) {
-    results[0] = "I'm sorry, " + userName + ". I'm afraid I can't do that.";
     results[1] = "img/bot_s.png";
+    results[2] = ""
+    results[3] = ""
   } else if (inputNumber.match(/0/)) {
-    results[0] = "Beep!";
     results[1] = "img/bot_l.png";
     results[2] = "BEEP!"
+    results[3] = ""
   } else if (inputNumber.match(/1/)) {
-    results[0] = "Boop!";
     results[1] = "img/bot_r.png";
+    results[2] = ""
     results[3] = "BOOP!"
   } else {
-    var numberPrints = [];
-    for (i = 0; i <= parseInt(inputNumber); i++) {
-      numberPrints[i] = " " + i;
-    }
-    results[0] = numberPrints.slice();
     results[1] = "img/bot_n.png";
+    results[2] = ""
+    results[3] = ""
   }
+
+  // number print
+  for (ip = 0; ip < numberPrints.length; ip++){
+    if (numberPrints[ip] === 0) {
+      numberPrints[ip] = "Beep!";
+    } else if (numberPrints[ip] % 3 === 0) {
+      numberPrints[ip] = "I'm sorry, " + userName + ". I'm afraid I can't do that.";
+    } else if (numberPrints[ip].toString().match(/0/)) {
+      numberPrints[ip] = "Beep!";
+    } else if (numberPrints[ip].toString().match(/1/)) {
+      numberPrints[ip] = "Boop!";
+    } else {}
+  }
+  results[0] = numberPrints;
   return results;
 }
 
@@ -42,7 +60,10 @@ $(document).ready(function() {
     var inputNumber = $("#inputNumber").val();
 
     var result = countNumbers(inputName, inputNumber);
-    $("#beepBoopOutput").text(result[0]);
+    for (printIndex = 0; printIndex < result[0].length; printIndex++) {
+      $("#beepBoopOutput").append(result[0][printIndex] + "&#13;&#10;");
+    }
+    $("#beepBoopOutput").append("----------------------------------------------------------&#13;&#10;");
     $("#bot_left").text(result[2]);
     $("#bot_right").text(result[3]);
 
